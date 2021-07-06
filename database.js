34e605a7ea58b0f258f9b33d649af936e45f3a59
magalipujol <magalipuuujol@gmail.com>
2021-07-06 11:06:11 -0300

    changed style and added a function to add pages

---
+++ database.js
@@ -0,0 +1,127 @@
+let pages = [
+    {
+        sectionId: "webdev-content",
+        contents: [
+            {
+                link:"https://hackerthemes.com/bootstrap-cheatsheet/#bg-dark",
+                linkText: "Bootstrap cheatsheet",
+                description: "An interactive list of Bootstrap classes for version 4.3.1"
+            },
+            {
+                link:"https://grid.layoutit.com/",
+                linkText: "Grid Lay out it",
+                description: "Grid maker for CSS"
+            },
+            {
+                link: "https://web.dev/learn/",
+                linkText: "Wev.Dev",
+                description: "Explore our structured learning paths to discover everything you need to know about building for the modern web."
+            },
+            {
+                link: "https://purecss.io/",
+                linkText: "Pure.css",
+                description: "A set of small, responsive CSS modules that you can use in every web project."
+            },
+            {
+                link: "https://bulma.io/",
+                linkText: "Bulma",
+                description: "Bulma is a free, open source framework that provides ready-to-use frontend components that you can easily combine to build responsive web interfaces."
+            }
+        ]
+    },
+    {
+        sectionId: "webdev-design-content",
+        contents: [
+            {link: "https://www.magicpattern.design/",
+             linkText: "Magic Pattern",
+             description: "Generate unlimited high quality background graphics."
+            },
+            {
+                link: "https://unsplash.com/t/interiors",
+                linkText: "Unsplash",
+                description: "Good quality free images, and it has a great API"
+            },
+            {
+                link: "https://undraw.co/",
+                linkText: "Undraw",
+                description: "Open-source illustrations for any idea you can imagine and create."
+            },
+            {
+                link: "https://favicon.io/",
+                linkText: "FavIcon",
+                description: "Quickly generate your favicon from text, image, or choose from hundreds of emojis."
+            },
+            {
+                link: "https://fontawesome.com/icons?d=gallery&p=2",
+                linkText: "Font Awesome Icons",
+                description: "The world's most popular and easiest to use icon set."
+            },
+            {
+                link: "https://animate.style/",
+                linkText: "Animate.css",
+                description: "Animate.css is a library of ready-to-use, cross-browser animations for use in your web projects. Great for emphasis, home pages, sliders, and attention-guiding hints."
+            },
+            {
+                link: "https://www.opendoodles.com/?ref=hiddentools",
+                linkText: "Open Doodles",
+                description: "A Free Set of Open-Source Illustrations!"
+            }
+        ]
+    },
+    {
+        sectionId: "tutorial-content",
+        contents: [
+            {
+                link: "https://www.youtube.com/watch?v=3PHXvlpOkf4",
+                linkText: "15 JavaScript Projects",
+                description: "Sharpen your JavaScript skills by building 15 projects using plain JavaScript without frameworks"
+            },
+            {
+                link: "https://www.dunebook.com/javascript-project-ideas-for-beginners/",
+                linkText: "21 JavaScript Projects",
+                description: "21 Javascript projects for begginers"
+            },
+            {
+                link: "https://www.freecodecamp.org/news/build-a-portfolio-with-vuejs/#introduction",
+                linkText: "Build and deploy a portfolio",
+                description: "How to Build and Deploy a Portfolio with Vue.js Axios, the GitHub REST API, and Netlify"
+            }
+        ]
+    },
+    {
+        sectionId: "programming-content",
+        contents: [
+            {
+                link: "https://learngitbranching.js.org/?locale=es_AR",
+                linkText: "Git branching",
+                description: "Learn Git branching"
+            },
+            {
+                link: "https://www.theodinproject.com/",
+                linkText: "The Oding project",
+                description: "The free and open source curriculum for learning modern web development."
+            },
+            {
+                link: "https://tiny.school/",
+                linkText: "Tiny schools",
+                description: "Tinyschool helps developers learn new skills, get mentorship, build real-world projects and find a job in top remote teams. "
+            },
+            {
+                link: "https://devdocs.io/",
+                linkText: "Dev Docs",
+                description: "DevDocs combines multiple API documentations in a fast, organized, and searchable interface."
+            }
+        ]
+    },
+    {
+        sectionId: "mix-content",
+        contents: [
+            {
+                link: "https://alternativeto.net/",
+                linkText: "Alternative to",
+                description: "Reviews, comments and recommendations from users like yourself."
+            }
+        ]
+    }
+
+]
---
+++ script.js
@@ -0,0 +1,30 @@
+{/* <a class="lead" target="_blank" href="https://purecss.io/">Pure.css </a>
+<div class="hidden">A set of small, responsive CSS modules that you can use in every web project.</div><br> */}
+
+
+
+
+function addWebpage (sectionID, link, linkText, description) {
+    var newLink = document.createElement("a")
+    var newDescription = document.createElement("div");
+    newLink.href = link
+    newLink.target ="_blank"
+    newLink.innerHTML = linkText
+    newLink.classList.add("lead")
+    newDescription.innerHTML = description 
+    newDescription.classList.add("hidden")
+    document.getElementById(sectionID).appendChild(newLink)
+    document.getElementById(sectionID).appendChild(newDescription)
+    document.getElementById(sectionID).appendChild(document.createElement('br'))
+    
+}
+
+// addWebpage ("webdev-content", "https://purecss.io/", "Pure.css", "A set of small, responsive CSS modules that you can use in every web project.")
+// addWebpage ("webdev-content", "https://purecss.io/", "Pure.css", "A set of small, responsive CSS modules that you can use in every web project.")
+
+
+for (let section of pages) {
+    for (let content of section.contents) {
+        addWebpage(section.sectionId, content.link, content.linkText, content.description)
+    }
+}
